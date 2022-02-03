68bb4660d4b060f7e9ee4c752276814c2d21cf3c
Xander Nguyen <xander.nguyen313@gmail.com>
2022-02-03 13:45:19 -0600

    Add more response media query for mobile

--- src/app/home/random-color.directive.ts
+++ src/app/home/random-color.directive.ts
@@ -4,13 +4,30 @@ import { Directive, ElementRef, OnInit} from '@angular/core';
   selector: '[appRandomColor]'
 })
 export class RandomColorDirective implements OnInit {
+  colorList = [
+    '#065535',
+    '#ffc0cb',
+    '#ff7373',
+    '#ffb6c1',
+    '#003366',
+    '#f08080',
+    '#ffc3a0',
+    '#468499',
+    '#f6546a',
+    '#101010',
+    '#81d8d0',
+    '#000080',
+    '#990000',
+    '#088da5',
+    '#6897bb'
+  ]
   constructor(private element: ElementRef) { }
 
   ngOnInit(){
-    this.element.nativeElement.style.backgroundColor = this.getRandomColor();
+    this.element.nativeElement.style.backgroundColor = this.colorList[this.getRandomColor()];
   }
 
   getRandomColor(){
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
+    return Math.floor(Math.random() * this.colorList.length);
   }
 }
